49ad5aac1a598b9a89548db4fa8ef1861dcbcd92
Aditya <lapratomo24@yahoo.com>
2024-03-14 15:55:48 +0100

    operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,37 +1,53 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 const output = (operator, initialNum, userNum) => {
     const calcDescription = `${initialNum} ${operator} ${userNum}`;
     outputResult(currentResult, calcDescription);
 }
 
+const writeLog = (identifier, initial, calcNumber, result) => {
+    const logEntry = {
+        operation: identifier,
+        initial: initial,
+        calculation: calcNumber,
+        result: result
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 const add = () => {
     const inputNum = parseInt(userInput.value);
     const initialResult = currentResult;
-    currentResult = currentResult + inputNum;
+    currentResult += inputNum;
     output('+', initialResult, inputNum);
+    writeLog('add', initialResult, inputNum, currentResult);
 }
 
 const subtract = () => {
     const inputNum = parseInt(userInput.value);
     const initialNum = currentResult;
-    currentResult = currentResult - inputNum;
+    currentResult -= inputNum;
     output('-', initialNum, inputNum);
+    writeLog('subtract', initialNum, inputNum, currentResult);
 }
 
 const multiply = () => {
     const inputNum = parseInt(userInput.value);
     const initialNum = currentResult;
-    currentResult = currentResult * inputNum;
+    currentResult *= inputNum;
     output('*', initialNum, inputNum);
+    writeLog('multiply', initialNum, inputNum, currentResult);
 }
 
 const divide = () => {
     const inputNum = parseInt(userInput.value);
     const initialNum = currentResult;
-    currentResult = currentResult / inputNum;
+    currentResult /= inputNum;
     output('/', initialNum, inputNum);
+    writeLog('divide', initialNum, inputNum, currentResult);
 }
 
 // currentResult = add(2,2);
